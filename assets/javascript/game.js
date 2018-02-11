59f867a872c42f69fab77ef25eba57f07704a483
Xandromus <xanderrapstine@gmail.com>
2018-02-11 10:50:41 -0600

    added IFFE function and created initial array

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+(function() {
+    var computerChoices = ["darth vader", "darth sidious", "darth maul", "darth tyranus", "princess leia", "luke skywalker", "lando calrissian", "anakin skywalker", "padme amidala", "obi-wan kenobi", "yoda", "qui-gon jinn", "chewbacca", "jar jar binks", "han solo", "boba fett"];
+
+
+
+
+})();
