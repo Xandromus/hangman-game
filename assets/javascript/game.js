9e25e386a1f3e5b3365f903cfc086fbd72e08ef3
Xandromus <xanderrapstine@gmail.com>
2018-02-11 11:04:17 -0600

    added random selection of character from array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 (function() {
-    var computerChoices = ["darth vader", "darth sidious", "darth maul", "darth tyranus", "princess leia", "luke skywalker", "lando calrissian", "anakin skywalker", "padme amidala", "obi-wan kenobi", "yoda", "qui-gon jinn", "chewbacca", "jar jar binks", "han solo", "boba fett"];
+    var starWarsCharacters = ["darth vader", "darth sidious", "darth maul", "darth tyranus", "princess leia", "luke skywalker", "lando calrissian", "anakin skywalker", "padme amidala", "obi-wan kenobi", "yoda", "qui-gon jinn", "chewbacca", "jar jar binks", "han solo", "boba fett"];
 
+    // Randomly chooses a choice from the starWarsCharacter array
+      var starWarsRandom = starWarsCharacters[Math.floor(Math.random() * starWarsCharacters.length)];
+      console.log(starWarsRandom);
 
 
 
