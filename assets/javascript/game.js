7cf196a9619d7d9882cbe867503b3921a0cf9751
Xandromus <xanderrapstine@gmail.com>
2018-02-11 17:58:08 -0600

    added lives and counter and fixed some keyup functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,17 @@
     window.onload = function() {
         var wins = 0;
         var letter;
+        var incorrectGuess;
         var starWarsCharacters = ["darth vader", "darth sidious", "darth maul", "darth tyranus", "princess leia", "luke skywalker", "lando calrissian", "anakin skywalker", "padme amidala", "obi-wan kenobi", "yoda", "qui-gon jinn", "chewbacca", "jar jar binks", "han solo", "boba fett"];
 
         // Randomly chooses a choice from the starWarsCharacters array
         var starWarsRandom = starWarsCharacters[Math.floor(Math.random() * starWarsCharacters.length)];
         console.log(starWarsRandom);
         var nameDisplay = [];
+        var counter = starWarsRandom.length;
+        var lives;
 
-        result = function() {
+        display = function() {
             nameHolder = document.getElementById('hold');
             letterList = document.createElement('ul');
 
@@ -30,20 +33,34 @@
                 letterList.appendChild(letter);
             }
         }
-        
+
+        guesses = function() {
+          lives = 10;
+          guessesRemaining = document.getElementById('guesses-remaining');
+          guessesRemaining.innerHTML = "<p>Number of Guesses Remaining: " + lives + "</p>";
+        }
+
 
         document.onkeyup = function(event) {
+          
 
-      // Determines which key was pressed.
-      var userGuess = event.key;
-
-      for (var i = 0; i < starWarsRandom.length; i++) {
-        if (starWarsRandom[i] === userGuess) {
-          nameDisplay[i].innerHTML = userGuess;
-          console.log(userGuess);
+            // Determines which key was pressed.
+            var userGuess = event.key;
+                for (var i = 0; i < starWarsRandom.length; i++) {
+                  if ((counter > 1) && (lives > 0)) {
+                    if ((starWarsRandom[i] === userGuess) && (userGuess != " ")) {
+                        nameDisplay[i].innerHTML = userGuess;
+                        counter -= 1;
+                        console.log(counter);
+                    }
+                  }
+                }
         }
-      }
-    }
-    result();
+
+        display();
+        guesses();
+        console.log(counter);
+        console.log(lives);
+
     }
 })();
